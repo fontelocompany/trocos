c7da97ad0babb56c92de697bad3f24842877d296
cordiogo <80708978+cordiogo@users.noreply.github.com>
2024-10-11 12:38:04 +0100

    v0.0.10-alpha
    - gitignore fix

---
+++ frontend/lib/types/auth.ts
@@ -0,0 +1,11 @@
+export interface User {
+    id: number;
+    email: string;
+    username: string;
+  }
+  
+  export interface AuthState {
+    user: User | null;
+    accessToken: string | null;
+    refreshToken: string | null;
+  }
